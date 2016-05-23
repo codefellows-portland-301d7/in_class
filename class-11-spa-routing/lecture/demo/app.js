1fa2903132668e21f1d06453aa9bff3b5b6c6faf
Rick Patci <rpatci@gmail.com>
2016-05-23 16:50:26 -0700

    push demo code

--- class-11-spa-routing/lecture/demo/app.js
+++ class-11-spa-routing/lecture/demo/app.js
@@ -1,1 +1,15 @@
 /*  Begin suggested demo by page.js */
+page();
+page('/', index);
+page('/contact', contact);
+
+
+function index() {
+    document.querySelector('p')
+      .textContent = "Index page generated on the client!";
+};
+
+function contact() {
+  document.querySelector('p')
+    .textContent = "Contact page generated on the client!";
+};
