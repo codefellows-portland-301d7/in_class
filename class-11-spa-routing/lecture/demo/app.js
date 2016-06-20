23433377deb6d38fd4c533e44912ec61d9fb9d00
Rick Patci <rpatci@gmail.com>
2016-06-20 08:51:45 -0700

    blank app.js for demo

--- class-11-spa-routing/lecture/demo/app.js
+++ class-11-spa-routing/lecture/demo/app.js
@@ -1,15 +1,1 @@
-/*  Begin suggested demo by page.js */
-page();
-page('/', index);
-page('/contact', contact);
 
-
-function index() {
-    document.querySelector('p')
-      .textContent = "Index page generated on the client!";
-};
-
-function contact() {
-  document.querySelector('p')
-    .textContent = "Contact page generated on the client!";
-};
