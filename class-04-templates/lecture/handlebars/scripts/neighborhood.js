9617bdcfab61d278949960a74caf429ea0b7a9fc
Aaron Krause <aaroncrows@Aarons-MacBook-Pro.local>
2016-12-19 10:33:54 -0800

    start demo

---
+++ class-04-templates/lecture/handlebars/scripts/neighborhood.js
@@ -0,0 +1,18 @@
+var neighborhoods = [];
+
+function Neighborhood (opts) {
+  for (key in opts) {
+    this[key] = opts[key];
+  }
+};
+
+Neighborhood.prototype.toHtml = function() {
+};
+
+neighborhoodDataSet.forEach(function(neighborhoodObject) {
+  neighborhoods.push(new Neighborhood(neighborhoodObject));
+});
+
+neighborhoods.forEach(function(ourNewNeighborhoodObject){
+  $('#neighborhoods').append(ourNewNeighborhoodObject.toHtml());
+});
